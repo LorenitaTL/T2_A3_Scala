de450101540e7b224b4bd256b8f8d0f3fbd81ac9
Lorena Trujillo Landeros <39895972+LorenitaTL@users.noreply.github.com>
2019-03-11 00:07:22 -0600

    Update Palindromas.scala
    Palindromas

--- Palindromas.scala
+++ Palindromas.scala
@@ -1,5 +1,21 @@
-
+import scala.collection.mutable.ListBuffer
 
 object Palindromas {
+  val lista = List("ala","cama", "oro","rosa", "oso")
+  var lista2 = ListBuffer[String]()
+  
+  def palindromas():Unit={
+    
+    for(x<-0 to lista.length-1){
+      lista2 += lista(x).reverse
+      if(lista(x)==lista2(x)){
+        println(lista2(x))
+      }
+    }
+  }
+  
+  def main(args: Array[String]): Unit = {
+    palindromas()
+  }
   
 }
