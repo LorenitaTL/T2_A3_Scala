7138c445690a7470a35763230e95a3a0549ef2c4
Lorena Trujillo Landeros <39895972+LorenitaTL@users.noreply.github.com>
2019-03-11 08:13:57 -0600

    Update Capicuas.scala

--- Capicuas.scala
+++ Capicuas.scala
@@ -1,5 +1,21 @@
-
+import scala.collection.mutable.ListBuffer
 
 object Capicuas {
+  val lista = List(452,323,211,242,67876,6116)
+  var lista2 = ListBuffer[String]()
+  
+  def capicuas():Unit={
+    
+    for(x<-0 to lista.length-1){
+      lista2 += lista(x).toString().reverse
+      if(lista(x).toString()==lista2(x)){
+        println(lista2(x))
+      }
+    }
+  }
+  
+  def main(args: Array[String]): Unit = {
+    capicuas()
+  }
   
 }
